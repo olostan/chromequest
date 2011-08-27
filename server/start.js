5df9f0e2079ae63e7eda57fb696edee168a11700
Arsen Kostenko <kosty@airsnake.grammarly.com>
2011-08-27 15:46:21 +0300

    Intermediate

--- server/start.js
+++ server/start.js
@@ -1,7 +1,11 @@
-var express = require("express"),
-    admin   = require("./admin.js");
+var express =  require("express"),
+    master   = require("./quest_master.js"),
+    player   = require("./quest_player.js");
 
 var app = express.createServer();
+
+master.register(app);
+player.register(app);
 
 app.get('/', function(req, res){
     res.send('Hello World');
@@ -15,9 +19,7 @@ app.get('/next-hash', function(req, res) {
     res.send({hash: hash});
 });
 
-app.get('/admin/:action', admin.action);
-app.get('/admin', admin.home);
-
 var port = 8080;
 console.log("Started server at "+port)
 app.listen(port);
+
