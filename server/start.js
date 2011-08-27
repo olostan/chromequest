c5dcd3e296066d00a98df6d1de8e840399e70595
Valentyn Shybanov <olostan@gmail.com>
2011-08-27 15:31:47 +0300

    player file

--- server/start.js
+++ server/start.js
@@ -6,6 +6,12 @@ var app = express.createServer();
 app.get('/', function(req, res){
     res.send('Hello World');
 });
+var quest = {
+    phases : [
+        { url: "http://google.com.ua/" },
+        { url: "https://github.com/" }
+    ]
+}
 
 app.get('/next-hash', function(req, res) {
     var hasher = crypto.createHash("md5");
