cd381c1ebd870eac580de0500014d7ccc4683175
Dmitry Fal <fal.dmitry@gmail.com>
2011-08-27 20:25:28 +0300

    fix state change

--- extension/js/default.js
+++ extension/js/default.js
@@ -2,10 +2,15 @@ $(document).ready(function(){
 
     $('#createQuest').click(function (){
         var action="master/create-quest";
-        var url = config.serverUrl + action;
+        var url = config.serverUrl + action + "?master=" + config.nick;
         $.getJSON(url, function callback(data) {
-            var bgp = chrome.extension.getBackgroundPage();
-            bgp.state = States.CREATED;
+            debugger;
+            if (data.questhash){
+                var bgp = chrome.extension.getBackgroundPage();
+                bgp.currentQuest = data.questhash;
+                bgp.setState(States.CREATED);
+
+            }
             console.log(data);
         });
     });
@@ -15,7 +20,7 @@ $(document).ready(function(){
         var url = config.serverUrl + action + "?q=" + $('#hash').val();
         $.getJSON(url, function callback(data) {
             var bgp = chrome.extension.getBackgroundPage();
-            bgp.state = States.JOINED;
+            bgp.setState(States.JOINED);
             console.log(data);
         });
     });
